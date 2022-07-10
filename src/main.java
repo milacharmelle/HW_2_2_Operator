446883c512159ad4ed207abc7b7ef78997c23141
Your Name <102684272+milacharmelle@users.noreply.github.com>
2022-07-10 18:31:21 +0500

    git push -u https://github.com/milacharmelle/HW_2_2_Operator.git

--- src/main.java
+++ src/main.java
@@ -6,23 +6,14 @@ public class main {
         int sum = balans + pay;
 
         if (pay >= 1_000) {
-            bonus = pay/100;
-        }
-        else {
+            bonus = pay / 100;
+        } else {
             bonus = 0;
         }
         int abonentBalans = balans + pay + bonus;
 
         System.out.println("Баланс: " + abonentBalans);
         System.out.println("Бонус: " + bonus);
-
-        // Условным оператором проверяете превысила ли
-        // сумма пополнения порог и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
-
-
-
 
 
     }
